198465eb6ec7232867ed2856c69952fd667a1dc8
santo0 <lemarti3472@gmail.com>
2021-11-08 17:31:40 +0100

    Main activity supports bottom navigation bar.

--- app/src/main/java/com/example/meet_n_music/MainActivity.java
+++ app/src/main/java/com/example/meet_n_music/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.meet_n_music;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNav;
+    NavController navController;
+    NavHostFragment navHostFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navhostfragment);
+        navController = navHostFragment.getNavController();
+        bottomNav = findViewById(R.id.bottom_navigation);
+        NavigationUI.setupWithNavController(bottomNav, navController);
+
     }
+
 }
