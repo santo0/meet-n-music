c1cdece9f750d7d6a6c50c7b7e7f9ad0dd562d72
santo0 <lemarti3472@gmail.com>
2021-11-10 10:40:45 +0100

    Top bar shet

--- app/src/main/java/com/example/meet_n_music/MainActivity.java
+++ app/src/main/java/com/example/meet_n_music/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.meet_n_music;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -13,18 +15,20 @@ public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNav;
     NavController navController;
     NavHostFragment navHostFragment;
-
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mToolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(mToolbar);
         navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navHostFragment);
         navController = navHostFragment.getNavController();
         bottomNav = findViewById(R.id.bottom_navigation);
         NavigationUI.setupWithNavController(bottomNav, navController);
 
+        ((ImageButton) mToolbar.findViewById(R.id.btn_create_event)).setOnClickListener(view -> {navController.navigate(R.id.action_feedFragment_to_creatingEventFragment);});
     }
 
 }
