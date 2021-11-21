a9e282b71f26cd6aa21f22c1b4297c20ef16a92b
santo0 <lemarti3472@gmail.com>
2021-11-21 18:56:05 +0100

    Feed fragment user management.

--- app/src/main/java/com/example/meet_n_music/FeedFragment.java
+++ app/src/main/java/com/example/meet_n_music/FeedFragment.java
@@ -15,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.meet_n_music.model.Event;
 import com.example.meet_n_music.repository.Repo;
 import com.example.meet_n_music.viewmodel.EventViewModel;
 import com.google.firebase.auth.FirebaseAuth;
@@ -26,6 +27,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 
 public class FeedFragment extends Fragment {
 
@@ -33,6 +36,7 @@ public class FeedFragment extends Fragment {
     private EventListAdapter adapter;
     private EventViewModel eventViewModel;
     private Button syncBtn;
+    private User loggedUser;
 
     public FeedFragment() {
         // Required empty public constructor
@@ -58,28 +62,31 @@ public class FeedFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_feed, container, false);
 
 
-        FirebaseUser fUser = FirebaseAuth.getInstance().getCurrentUser();
-        DatabaseReference reference  =FirebaseDatabase.getInstance().getReference();
-        Query query = reference.child("Users/"+fUser.getUid());
-        Log.d("debug", fUser.getUid());
-        ValueEventListener valueEventListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Log.d("debug", snapshot.toString());
-                TextView tvUname = view.findViewById(R.id.tvUname);
-                User user = snapshot.getValue(User.class);
-                Log.d("debug", user.username);
-                Log.d("debug", user.email);
 
+
+        TextView tvUname = view.findViewById(R.id.tvUname);
+        User user = getLoggedUser(new OnGetDataListener() {
+            @Override
+            public void onSuccess(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                loggedUser = user;
+                Log.d("debug", loggedUser.username);
+                Log.d("debug", loggedUser.email);
                 tvUname.setText(user.username);
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
+            public void onStart() {
+                Log.d("ONSTART", "Started");
             }
-        };
-        query.addListenerForSingleValueEvent(valueEventListener);
+
+            @Override
+            public void onFailure() {
+                Log.d("onFailure", "Failed");
+            }
+        });
+
+
 
         recyclerView = view.findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
@@ -96,5 +103,40 @@ public class FeedFragment extends Fragment {
         syncBtn.setOnClickListener(view1 -> Repo.getInstance().getEvents());
 
         return view;
+    }
+
+
+    public User getLoggedUser(OnGetDataListener listener) {
+        FirebaseUser fbUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (fbUser != null) {
+            listener.onStart();
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+            String uid = fbUser.getUid();
+            Query query = reference.child("Users/" + uid);
+            Log.d("debug", uid);
+            ValueEventListener valueEventListener = new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    Log.d("debug", snapshot.toString());
+                    listener.onSuccess(snapshot);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    listener.onFailure();
+                }
+            };
+            query.addListenerForSingleValueEvent(valueEventListener);
+        } else {
+            loggedUser = null;
+        }
+        return loggedUser;
+    }
+
+    public interface OnGetDataListener {
+        //this is for callbacks
+        void onSuccess(DataSnapshot dataSnapshot);
+        void onStart();
+        void onFailure();
     }
 }
