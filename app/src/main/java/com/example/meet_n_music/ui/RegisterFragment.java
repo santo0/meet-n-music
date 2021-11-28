aa1055f2b3845c117d34a104079d1153bbd81f5a
arnau <amolins27@gmail.com>
2021-11-28 13:06:27 +0100

    Register genre, and Genre searcher started.

--- app/src/main/java/com/example/meet_n_music/ui/RegisterFragment.java
+++ app/src/main/java/com/example/meet_n_music/ui/RegisterFragment.java
@@ -11,9 +11,11 @@ import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -38,6 +40,7 @@ public class RegisterFragment extends Fragment {
     private ProgressBar progressBar2;
     private Button registerUser;
     private FirebaseAuth mAuth;
+    private Spinner genreUser;
 
     @Override
     public void onResume() {
@@ -61,6 +64,10 @@ public class RegisterFragment extends Fragment {
         psswrd2 = (EditText) view.findViewById(R.id.password2);
         rPsswrd2 = (EditText) view.findViewById(R.id.repeatPassword2);
         progressBar2 = (ProgressBar) view.findViewById(R.id.progressBar2);
+        genreUser = (Spinner) view.findViewById(R.id.genreUser);
+        ArrayAdapter<String> genreAdaptor = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Genres));
+        genreAdaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genreUser.setAdapter(genreAdaptor);
         registerUser = (Button) view.findViewById(R.id.registerUser);
         registerUser.setOnClickListener(v -> {
             registerUserFunction();
@@ -74,6 +81,7 @@ public class RegisterFragment extends Fragment {
         String userString = username2.getText().toString().trim();
         String psswrdString = psswrd2.getText().toString().trim();
         String rPsswrdString = rPsswrd2.getText().toString().trim();
+        String genreUserString = genreUser.getSelectedItem().toString().trim();
 
         if (userString.isEmpty()) {
             username2.setError("Username is required!");
@@ -114,7 +122,7 @@ public class RegisterFragment extends Fragment {
         progressBar2.setVisibility(View.VISIBLE);
 
         AuthViewModel authViewModel = new ViewModelProvider(this).get(AuthViewModel.class);
-        authViewModel.signUp(userString, "Rock", emailString, psswrdString).observe(getViewLifecycleOwner(), new Observer<User>() {
+        authViewModel.signUp(userString, genreUserString, emailString, psswrdString).observe(getViewLifecycleOwner(), new Observer<User>() {
             @Override
             public void onChanged(User user) {
                 if (user != null) {
--- app/src/main/java/com/example/meet_n_music/ui/SearchFragment.java
+++ app/src/main/java/com/example/meet_n_music/ui/SearchFragment.java
@@ -3,18 +3,38 @@ package com.example.meet_n_music.ui;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
+import com.example.meet_n_music.EventListAdapter;
 import com.example.meet_n_music.R;
+import com.example.meet_n_music.model.Event;
+import com.example.meet_n_music.model.User;
+import com.example.meet_n_music.viewmodel.AuthViewModel;
+import com.example.meet_n_music.viewmodel.FeedViewModel;
+
+import java.util.ArrayList;
 
 
 public class SearchFragment extends Fragment {
 
-    public SearchFragment() {
+    private Spinner genreSearcher;
+    private Button searchButtonGenre;
+    private RecyclerView recyclerViewGenre;
+    private EventListAdapter adapterEventGenreList;
+    AuthViewModel authViewModel;
+    FeedViewModel feedViewModel;
 
+    public SearchFragment() {
     }
 
     @Override
@@ -24,9 +44,36 @@ public class SearchFragment extends Fragment {
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        getActivity().findViewById(R.id.bottom_navigation).setVisibility(View.VISIBLE);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        genreSearcher = (Spinner) view.findViewById(R.id.genreSearch);
+        ArrayAdapter<String> genreAdaptor = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Genres));
+        genreAdaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genreSearcher.setAdapter(genreAdaptor);
+        authViewModel = new ViewModelProvider(this).get(AuthViewModel.class);
+        feedViewModel = new ViewModelProvider(this).get(FeedViewModel.class);
+        recyclerViewGenre = view.findViewById(R.id.recyclerViewGenre);
+        recyclerViewGenre.setHasFixedSize(true);
+        recyclerViewGenre.setLayoutManager(new LinearLayoutManager(getContext()));
+        User user = authViewModel.getCurrentUser().getValue();
+        searchButtonGenre = (Button) view.findViewById(R.id.searchButtonGenre);
+        searchButtonGenre.setOnClickListener(l -> {
+            listEventGenre(user);
+        });
+        return view;
+    }
+
+    public void listEventGenre(User user){
+        adapterEventGenreList = new EventListAdapter();
+        MutableLiveData<ArrayList<Event>> eventsToShow = feedViewModel.getEventMutableLiveData();
     }
 }
